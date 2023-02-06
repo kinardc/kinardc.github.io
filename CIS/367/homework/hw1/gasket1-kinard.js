be7c2c44b064fe35860d89b425e7a3241bd72bc9
kinardc <kinardc@github.com>
2023-02-06 08:46:17 -0500

    darkened canvas clear color to distinguish from window background

--- CIS/367/homework/hw1/gasket1-kinard.js
+++ CIS/367/homework/hw1/gasket1-kinard.js
@@ -69,7 +69,7 @@ window.onload = function init()
     //  Configure WebGL
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.clearColor( 0.9, 0.9, 0.9, 1.0 );
 
     //  Load shaders and initialize attribute buffers
 
--- CIS/367/homework/hw2.1/triangle-interact.js
+++ CIS/367/homework/hw2.1/triangle-interact.js
@@ -42,7 +42,7 @@ window.onload = function init() {
 
   // configure WebGL
   gl.viewport(0, 0, canvas.width, canvas.height);
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.clearColor(0.9, 0.9, 0.9, 1.0);
 
   // load shaders and initialize attribute buffers
   var program = initShaders(gl, 'vertex-shader', 'fragment-shader');
